e0c75387792f02e5ddedb9576922152e1ed01a00
Abdallah Mohamed <pedrito1172341@gmail.com>
2026-05-13 22:36:58 +0300

    Write remaining 3 Queries

--- DB-Project/Data/DbConnectionFactory.cs
+++ DB-Project/Data/DbConnectionFactory.cs
@@ -4,9 +4,8 @@ namespace DB_Project.Data;
 
 public class DbConnectionFactory(IConfiguration configuration)
 {
-    private readonly string _connectionString =
-        configuration.GetConnectionString("DefaultConnection")
-        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
+    // HARDCODE the string exactly as it is in Rider
+    private readonly string _connectionString = 
+        "Server=localhost,1433;Database=VetClinicDB;User Id=sa;Password=UbuntuDatabase2026;Encrypt=True;TrustServerCertificate=True;Connect Timeout=30;";
     public SqlConnection Create() => new(_connectionString);
 }
